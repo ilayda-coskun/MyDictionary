1e3bab110168fb11e43c33f5c5f0e9a61770ef1b
İlayda Coşkun <ilayda.coskun2001@hotmail.com>
2021-02-04 12:43:19 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<TKey, TValue>
+    {
+        TKey[] key;
+        TKey[] tempKey;
+
+        TValue[] value;
+        TValue[] tempValue;
+
+        public MyList() //Constructor   
+        {
+            key = new TKey[0];
+            value = new TValue[0];
+        }
+        public void Add(TKey keyItem, TValue valueItem)
+        {
+            tempKey = key;
+            key = new TKey[key.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+            }
+
+            key[key.Length - 1] = keyItem;
+
+            tempValue = value;
+            value = new TValue[value.Length + 1];
+
+            for (int j = 0; j < tempKey.Length; j++)
+            {
+                value[j] = tempValue[j];
+            }
+
+            value[value.Length - 1] = valueItem;
+        }
+
+        public TKey[]  Key { get { return key; } }
+
+        public TValue[] Value { get { return value; } }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        private const string V = "";
+
+        static void Main(string[] args)
+        {
+            MyList<int, string> customer = new MyList<int, string>();
+
+            customer.Add(1, "İlayda");
+            customer.Add(2, "Engin");
+            customer.Add(3, "Melek");
+            customer.Add(4, "Ahmet");
+
+            Console.WriteLine("---C# Dictionary İle Müşteri Ekleme---\n");
+
+            for (int i = 0; i < customer.Key.Length; i++)
+            {
+                Console.WriteLine("\tAnahtar : " + customer.Key[i] + "\n\tDeğer : " + customer.Value[i] + "\n");
+            }
+        }
+    }
+}
